12472d0229db9fd39ba24a04f4be53aa7243d452
2003lgx <47873499+2003lgx@users.noreply.github.com>
2023-03-14 18:57:49 +0800

    Update tracker.sh

--- tracker.sh
+++ tracker.sh
@@ -49,7 +49,12 @@ GET_TRACKERS() {
     if [[ -z "${CUSTOM_TRACKER_URL}" ]]; then
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers..."
         TRACKER=$(
-            ${DOWNLOADER} https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/ATaria2_all.txt 
+            ${DOWNLOADER} https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/ATaria2_all.txt ||
+            ${DOWNLOADER} https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt ||
+            ${DOWNLOADER} https://trackerslist.com/all.txt ||
+            ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
+            ${DOWNLOADER} https://cdn.staticaly.com/gh/XIU2/TrackersListCollection@master/all_aria2.txt ||
+            ${DOWNLOADER} https://trackers.p3terx.com/all_aria2.txt
         )
     else
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers from url(s):${CUSTOM_TRACKER_URL} ..."
